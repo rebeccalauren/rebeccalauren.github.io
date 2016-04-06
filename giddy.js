dec866deb057258aba3416c111cc06e1ecff7bc6
Rebecca Lauren <rebeccalauren@gmail.com>
2016-04-06 11:10:20 -0400

    initial commit

---
+++ giddy.js
@@ -0,0 +1,37 @@
+$(document).ready(function() {
+        $("#pic").click(
+            function(){
+                $("#camera").animate({
+                opacity: 1
+         } , 1000 );
+                 });
+        $("#camelid").click(
+            function(){
+                $("#llama").animate({
+                opacity: 1
+         } , 1000 );
+                 });
+        $("#gustrudy").click(
+            function(){
+                $("#dog").animate({
+                opacity: 1
+         } , 1000 );
+                 });
+        $("#guzzle").click(
+            function(){
+                $("#wine").animate({
+                opacity: 1
+         } , 1000 );
+                 });
+         $("#martinez").click(
+            function(){
+                $("#monica").animate({
+                opacity: 1
+         } , 1000 );
+                $(".final").animate({
+                opacity: 1
+         } , 5000 );
+                 });
+ });
+   
+                    
